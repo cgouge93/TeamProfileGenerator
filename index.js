bd673b81222dd974476a1816d9405e8cf339f6f4
Carly G <73192129+cgouge93@users.noreply.github.com>
2021-02-21 19:35:11 -0600

    wrote addTeamMember functionality

--- index.js
+++ index.js
@@ -22,5 +22,58 @@ function addTeamMember() {
         ],
         name: "role"
     },
+    {
+        message: "Enter team member's ID",
+        name: "id"
+    },
+    {
+        message: "Enter team member's email",
+        name: "email"
+    }
 ])
+.then(function({name, role, id, email}) {
+    let roleInfo = "";
+    if (role === "Engineer") {
+        roleInfo = "GitHub username";
+    } else if (role === "Intern") {
+        roleInfo = "school name";
+    } else {
+        roleInfo = "office phone number";
+    }
+    inquirer.prompt([
+    {
+        message: `Enter team member's ${roleInfo}`,
+        name: "roleInfo"
+    },
+    {
+        type: "list",
+        message: "Would you like to add more team members?",
+        choices: [
+            "yes",
+            "no"
+        ],
+        name: "addMore"
+    }
+])
+.then(function({roleInfo, moreMembers}) {
+    let newMember;
+    if (role === "Engineer") {
+        newMember = new Engineer(name, id, email, roleInfo);
+    } else if (role === "Intern") {
+        newMember = new Intern(name, id, email, roleInfo);
+    } else {
+        newMember = new Manager(name, id, email, roleInfo);
+    }
+    employees.push(newMember);
+    addHtml(newMember)
+    .then(function() {
+        if (addMore === "yes"){
+            addTeamMember();
+        } else {
+            finishHtml();   
+        }
+        });
+    });
+});
 }
+
