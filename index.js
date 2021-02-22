4c24cb91d11398e8e3b7b021596536c9501b431b
Carly G <73192129+cgouge93@users.noreply.github.com>
2021-02-21 20:05:20 -0600

    wrote classes and constructor functions

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const Engineer = require("./lib/engineer");
 const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
 
+
 const employees = [];
+
+function initApp() {
+    startHtml();
+    addTeamMember();
+}
 
 function addTeamMember() {
     inquirer.prompt([
@@ -55,7 +61,7 @@ function addTeamMember() {
         name: "addMore"
     }
 ])
-.then(function({roleInfo, moreMembers}) {
+.then(function({roleInfo, addMore}) {
     let newMember;
     if (role === "Engineer") {
         newMember = new Engineer(name, id, email, roleInfo);
@@ -77,6 +83,30 @@ function addTeamMember() {
 });
 }
 
+function startHtml() {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-dark mb-5">
+            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+        </nav>
+        <div class="container">
+            <div class="row">`;
+    fs.writeFile("./dist/team.html", html, function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log("start");
+}
+
 function addHtml(member) {
     return new Promise(function(resolve, reject) {
         const name = member.getName();
@@ -96,6 +126,52 @@ function addHtml(member) {
             </ul>
             </div>
             </div>`;
+        } else if (role === "Intern") {
+            const school = member.getSchool();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Intern</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email address: ${email}</li>
+                <li class="list-group-item">School: ${school}</li>
+            </ul>
+            </div>
+        </div>`;
+        } else {
+            const officePhone = member.getOfficeNumber();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Manager</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email address: ${email}</li>
+                <li class="list-group-item">Office phone: ${officePhone}</li>
+            </ul>
+            </div>
+        </div>`
         }
-    })
+        console.log("adding team member");
+        fs.appendFile("./dist/team.html", data, function(err) {
+            if (err) {
+                return reject(err);
+            };
+            return resolve();
+        });
+    });
 }
+
+function finishHtml() {
+    const html = ` </div>
+    </div>
+    </body>
+    </html>`;
+    fs.appendFile("./dist/team.html", html, function(err) {
+        if (err) {
+           console.log(err)
+        };
+    });
+    console.log("end");
+}
+
+initApp();
