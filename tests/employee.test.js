c1ed41925b96ee8bf301819797e287824005c231
Carly G <73192129+cgouge93@users.noreply.github.com>
2021-02-22 11:40:57 -0600

    wrote test case for employee class

---
+++ tests/employee.test.js
@@ -0,0 +1,60 @@
+  
+const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+    it("Creates new employee object", () => {
+        const e = new Employee();
+        expect(typeof(e)).toBe("object");
+    });
+
+    it("Can set name via constructor arguments", () => {
+        const name = "Jim Halpert";
+        const e = new Employee(name);
+        expect(e.name).toBe(name);
+    });
+
+    it("Can set id via constructor argument", () => {
+        const testValue = 100;
+        const e = new Employee("Foo", testValue);
+        expect(e.id).toBe(testValue);
+    });
+
+    it("Can set email via constructor argument", () => {
+        const testValue = "jim@dundermifflin.com";
+        const e = new Employee("Foo", 1, testValue);
+        expect(e.email).toBe(testValue);
+    });
+
+    describe("getName", () => {
+        it("Can get name via getName()", () => {
+            const testValue = "Jim Halpert";
+            const e = new Employee(testValue);
+            expect(e.getName()).toBe(testValue);
+        });
+    });
+        
+    describe("getId", () => {
+        it("Can get id via getId()", () => {
+            const testValue = 100;
+            const e = new Employee("Foo", testValue);
+            expect(e.getId()).toBe(testValue);
+        });
+    });
+        
+    describe("getEmail", () => {
+        it("Can get email via getEmail()", () => {
+            const testValue = "jim@dundermifflin.com";
+            const e = new Employee("Foo", 1, testValue);
+            expect(e.getEmail()).toBe(testValue);
+        });
+    });
+        
+    describe("getRole", () => {
+        it("getRole() should return \"Employee\"", () => {
+            const testValue = "Employee";
+            const e = new Employee("Alice", 1, "test@test.com");
+            expect(e.getRole()).toBe(testValue);
+        });
+    });
+    
+});
