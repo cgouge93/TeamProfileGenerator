623fba940fc9cd4c4f03c457033e0c3e0e4aad0f
Carly G <73192129+cgouge93@users.noreply.github.com>
2021-02-21 19:26:37 -0600

    added dependencies, began writing index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Engineer = require("./lib/engineer");
+const Intern = require("./lib/intern");
+const Manager = require("./lib/manager");
+
+const employees = [];
+
+function addTeamMember() {
+    inquirer.prompt([
+    {
+        message: "Enter team member's name",
+        name: "name"
+    },
+    {
+        type: "list",
+        message: "Select team member's role",
+        choices: [
+            "Engineer",
+            "Intern",
+            "Manager"
+        ],
+        name: "role"
+    },
+])
+}
